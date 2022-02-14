16f015d5508187eb7681f60c8b4a7a3de08a1342
Duy Nguyen <Duy.Nguyen@avispl.com>
2022-02-14 10:16:36 +0700

    SYAL-727
    Update QSCUndnemoCommunicatorTest file, add UTs for controlling part.

--- src/test/java/com/avispl/symphony/dal/avdevices/audionetworkinterface/qsc/undnemo/QSCUndnemoCommunicatorTest.java
+++ src/test/java/com/avispl/symphony/dal/avdevices/audionetworkinterface/qsc/undnemo/QSCUndnemoCommunicatorTest.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
 import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
+import com.avispl.symphony.dal.avdevices.audionetworkinterface.qsc.undnemo.utils.QSCUndnemoMetric;
 
 /**
  * Unit test for {@link QSCUndnemoCommunicator}.
@@ -61,26 +63,26 @@ class QSCUndnemoCommunicatorTest extends TestCase {
 		Assertions.assertEquals("1", stats.get("ButtonBrightness"));
 		Assertions.assertEquals("1", stats.get("Volume"));
 		Assertions.assertEquals("1", stats.get("SpeakerMute"));
-		Assertions.assertEquals("3", stats.get("(00)ActiveChannel#ChannelIndex"));
-		Assertions.assertEquals("MXA910-MillPark1-d", stats.get("(01)Channel 1" + "#" + "DeviceName"));
-		Assertions.assertEquals("1", stats.get("(01)Channel 1" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(01)Channel 1" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(01)Channel 1" + "#" + "DisplayName"));
+		Assertions.assertEquals("3", stats.get("ActiveChannel#ChannelIndex"));
+		Assertions.assertEquals("MXA910-MillPark1-d", stats.get("Channel 01" + "#" + "DeviceName"));
+		Assertions.assertEquals("1", stats.get("Channel 01" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 01" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 01" + "#" + "DisplayName"));
 
-		Assertions.assertEquals("MXA910-MillPark2-d", stats.get("(02)Channel 2" + "#" + "DeviceName"));
-		Assertions.assertEquals("1", stats.get("(02)Channel 2" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(02)Channel 2" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(02)Channel 2" + "#" + "DisplayName"));
+		Assertions.assertEquals("MXA910-MillPark2-d", stats.get("Channel 02" + "#" + "DeviceName"));
+		Assertions.assertEquals("1", stats.get("Channel 02" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 02" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 02" + "#" + "DisplayName"));
 
-		Assertions.assertEquals("3", stats.get("(00)ActiveChannel" + "#" + "ChannelIndex"));
-		Assertions.assertEquals("1", stats.get("(00)ActiveChannel" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(00)ActiveChannel" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(00)ActiveChannel" + "#" + "DisplayName"));
+		Assertions.assertEquals("3", stats.get("ActiveChannel" + "#" + "ChannelIndex"));
+		Assertions.assertEquals("1", stats.get("ActiveChannel" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("ActiveChannel" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("ActiveChannel" + "#" + "DisplayName"));
 
-		Assertions.assertEquals("", stats.get("(16)Channel 16" + "#" + "DeviceName"));
-		Assertions.assertEquals("0", stats.get("(16)Channel 16" + "#" + "EnableState"));
-		Assertions.assertEquals("", stats.get("(16)Channel 16" + "#" + "ChannelName"));
-		Assertions.assertEquals("No Channel Assigned", stats.get("(16)Channel 16" + "#" + "DisplayName"));
+		Assertions.assertEquals("", stats.get("Channel 16" + "#" + "DeviceName"));
+		Assertions.assertEquals("0", stats.get("Channel 16" + "#" + "EnableState"));
+		Assertions.assertEquals("", stats.get("Channel 16" + "#" + "ChannelName"));
+		Assertions.assertEquals("No Channel Assigned", stats.get("Channel 16" + "#" + "DisplayName"));
 	}
 
 	/**
@@ -103,22 +105,22 @@ class QSCUndnemoCommunicatorTest extends TestCase {
 		Assertions.assertEquals("1", stats.get("ButtonBrightness"));
 		Assertions.assertEquals("1", stats.get("Volume"));
 		Assertions.assertEquals("1", stats.get("SpeakerMute"));
-		Assertions.assertEquals("3", stats.get("(00)ActiveChannel#ChannelIndex"));
+		Assertions.assertEquals("3", stats.get("ActiveChannel#ChannelIndex"));
 
-		Assertions.assertEquals("MXA910-MillPark1-d", stats.get("(01)Channel 1" + "#" + "DeviceName"));
-		Assertions.assertEquals("1", stats.get("(01)Channel 1" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(01)Channel 1" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(01)Channel 1" + "#" + "DisplayName"));
+		Assertions.assertEquals("MXA910-MillPark1-d", stats.get("Channel 01" + "#" + "DeviceName"));
+		Assertions.assertEquals("1", stats.get("Channel 01" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 01" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 01" + "#" + "DisplayName"));
 
-		Assertions.assertEquals("MXA910-MillPark2-d", stats.get("(02)Channel 2" + "#" + "DeviceName"));
-		Assertions.assertEquals("1", stats.get("(02)Channel 2" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(02)Channel 2" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(02)Channel 2" + "#" + "DisplayName"));
+		Assertions.assertEquals("MXA910-MillPark2-d", stats.get("Channel 02" + "#" + "DeviceName"));
+		Assertions.assertEquals("1", stats.get("Channel 02" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 02" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 02" + "#" + "DisplayName"));
 
-		Assertions.assertEquals("3", stats.get("(00)ActiveChannel" + "#" + "ChannelIndex"));
-		Assertions.assertEquals("1", stats.get("(00)ActiveChannel" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(00)ActiveChannel" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(00)ActiveChannel" + "#" + "DisplayName"));
+		Assertions.assertEquals("3", stats.get("ActiveChannel" + "#" + "ChannelIndex"));
+		Assertions.assertEquals("1", stats.get("ActiveChannel" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("ActiveChannel" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("ActiveChannel" + "#" + "DisplayName"));
 	}
 
 	/**
@@ -141,20 +143,172 @@ class QSCUndnemoCommunicatorTest extends TestCase {
 		Assertions.assertEquals("1", stats.get("ButtonBrightness"));
 		Assertions.assertEquals("1", stats.get("Volume"));
 		Assertions.assertEquals("1", stats.get("SpeakerMute"));
-		Assertions.assertEquals("3", stats.get("(00)ActiveChannel#ChannelIndex"));
-		Assertions.assertEquals("MXA910-MillPark1-d", stats.get("(01)Channel 1" + "#" + "DeviceName"));
-		Assertions.assertEquals("1", stats.get("(01)Channel 1" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(01)Channel 1" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(01)Channel 1" + "#" + "DisplayName"));
+		Assertions.assertEquals("3", stats.get("ActiveChannel#ChannelIndex"));
+		Assertions.assertEquals("MXA910-MillPark1-d", stats.get("Channel 01" + "#" + "DeviceName"));
+		Assertions.assertEquals("1", stats.get("Channel 01" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 01" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 01" + "#" + "DisplayName"));
 
-		Assertions.assertEquals("MXA910-MillPark2-d", stats.get("(02)Channel 2" + "#" + "DeviceName"));
-		Assertions.assertEquals("1", stats.get("(02)Channel 2" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(02)Channel 2" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(02)Channel 2" + "#" + "DisplayName"));
+		Assertions.assertEquals("MXA910-MillPark2-d", stats.get("Channel 02" + "#" + "DeviceName"));
+		Assertions.assertEquals("1", stats.get("Channel 02" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 02" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("Channel 02" + "#" + "DisplayName"));
 
-		Assertions.assertEquals("3", stats.get("(00)ActiveChannel" + "#" + "ChannelIndex"));
-		Assertions.assertEquals("1", stats.get("(00)ActiveChannel" + "#" + "EnableState"));
-		Assertions.assertEquals("Automix Out", stats.get("(00)ActiveChannel" + "#" + "ChannelName"));
-		Assertions.assertEquals("Automix Out", stats.get("(00)ActiveChannel" + "#" + "DisplayName"));
+		Assertions.assertEquals("3", stats.get("ActiveChannel" + "#" + "ChannelIndex"));
+		Assertions.assertEquals("1", stats.get("ActiveChannel" + "#" + "EnableState"));
+		Assertions.assertEquals("Automix Out", stats.get("ActiveChannel" + "#" + "ChannelName"));
+		Assertions.assertEquals("Automix Out", stats.get("ActiveChannel" + "#" + "DisplayName"));
+	}
+
+	/**
+	 * Test controlProperty set button brightness
+	 * @throws Exception when fail to control
+	 */
+	@Tag("RealDevice")
+	@Test
+	void testControlPropertyButtonBrightness() throws Exception {
+		ControllableProperty property = new ControllableProperty();
+		property.setValue(1);
+		property.setProperty(QSCUndnemoMetric.BUTTON_BRIGHTNESS.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+		// getMultipleStatistics after control
+		qscUndnemoCommunicator.getMultipleStatistics();
+		// Sleep here to wait for worker thread to fetch channel info
+		Thread.sleep(30000);
+		ExtendedStatistics extendedStatistics = (ExtendedStatistics) qscUndnemoCommunicator.getMultipleStatistics().get(0);
+		Map<String, String> stats = extendedStatistics.getStatistics();
+		Assertions.assertEquals("1", stats.get("ButtonBrightness"));
+	}
+
+	/**
+	 * Test controlProperty set display brightness
+	 * @throws Exception when fail to control
+	 */
+	@Tag("RealDevice")
+	@Test
+	void testControlPropertyDisplayBrightness() throws Exception {
+		ControllableProperty property = new ControllableProperty();
+		property.setValue(1);
+		property.setProperty(QSCUndnemoMetric.DISPLAY_BRIGHTNESS.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+		// getMultipleStatistics after control
+		qscUndnemoCommunicator.getMultipleStatistics();
+		// Sleep here to wait for worker thread to fetch channel info
+		Thread.sleep(30000);
+		ExtendedStatistics extendedStatistics = (ExtendedStatistics) qscUndnemoCommunicator.getMultipleStatistics().get(0);
+		Map<String, String> stats = extendedStatistics.getStatistics();
+		Assertions.assertEquals("1", stats.get("DisplayBrightness"));
+	}
+
+	/**
+	 * Test controlProperty set volume
+	 * @throws Exception when fail to control
+	 */
+	@Tag("RealDevice")
+	@Test
+	void testControlPropertyVolume() throws Exception {
+		ControllableProperty property = new ControllableProperty();
+		property.setValue(1);
+		property.setProperty(QSCUndnemoMetric.VOLUME.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+		// getMultipleStatistics after control
+		qscUndnemoCommunicator.getMultipleStatistics();
+		// Sleep here to wait for worker thread to fetch channel info
+		Thread.sleep(30000);
+		ExtendedStatistics extendedStatistics = (ExtendedStatistics) qscUndnemoCommunicator.getMultipleStatistics().get(0);
+		Map<String, String> stats = extendedStatistics.getStatistics();
+		Assertions.assertEquals("1", stats.get("Volume"));
+	}
+
+	/**
+	 * Test controlProperty set speaker mute
+	 * @throws Exception when fail to control
+	 */
+	@Tag("RealDevice")
+	@Test
+	void testControlPropertySpeakerMute() throws Exception {
+		ControllableProperty property = new ControllableProperty();
+		property.setValue(1);
+		property.setProperty(QSCUndnemoMetric.SPEAKER_MUTE.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+		// getMultipleStatistics after control
+		qscUndnemoCommunicator.getMultipleStatistics();
+		// Sleep here to wait for worker thread to fetch channel info
+		Thread.sleep(30000);
+		ExtendedStatistics extendedStatistics = (ExtendedStatistics) qscUndnemoCommunicator.getMultipleStatistics().get(0);
+		Map<String, String> stats = extendedStatistics.getStatistics();
+		Assertions.assertEquals("1", stats.get("SpeakerMute"));
+	}
+
+	/**
+	 * Test controlProperty set active channel index
+	 * - Case one: given index is the same as current active channel index.
+	 * @throws Exception when fail to control
+	 */
+	@Tag("RealDevice")
+	@Test
+	void testControlPropertyActiveChannelCaseOne() throws Exception {
+		qscUndnemoCommunicator.getMultipleStatistics();
+		// Sleep here to wait for worker thread to fetch channel info
+		Thread.sleep(30000);
+		qscUndnemoCommunicator.getMultipleStatistics();
+		ControllableProperty property = new ControllableProperty();
+		property.setValue(3);
+		property.setProperty(QSCUndnemoMetric.ACTIVE_CHANNEL_INDEX.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+		ExtendedStatistics extendedStatistics = (ExtendedStatistics) qscUndnemoCommunicator.getMultipleStatistics().get(0);
+		Map<String, String> stats = extendedStatistics.getStatistics();
+		Assertions.assertEquals("3", stats.get("ActiveChannel#ChannelIndex"));
+	}
+
+	/**
+	 * Test controlProperty set active channel index
+	 * - Case two: given index is different from current active channel index.
+	 * @throws Exception when fail to control
+	 */
+	@Tag("RealDevice")
+	@Test
+	void testControlPropertyActiveChannelCaseTwo() throws Exception {
+		qscUndnemoCommunicator.getMultipleStatistics();
+		// Sleep here to wait for worker thread to fetch channel info
+		Thread.sleep(30000);
+		qscUndnemoCommunicator.getMultipleStatistics();
+		ControllableProperty property = new ControllableProperty();
+		property.setValue(4);
+		property.setProperty(QSCUndnemoMetric.ACTIVE_CHANNEL_INDEX.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+		ExtendedStatistics extendedStatistics = (ExtendedStatistics) qscUndnemoCommunicator.getMultipleStatistics().get(0);
+		Map<String, String> stats = extendedStatistics.getStatistics();
+		Assertions.assertEquals("4", stats.get("ActiveChannel#ChannelIndex"));
+		// Set it back to 3, so that it won't conflict other tests.
+		property.setValue(3);
+		property.setProperty(QSCUndnemoMetric.ACTIVE_CHANNEL_INDEX.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+	}
+
+	/**
+	 * Test controlProperty get all stats
+	 * Expect controlProperty successfully channel index: 1,2
+	 */
+	@Tag("RealDevice")
+	@Test
+	void testControlPropertyActiveChannelCaseThree() throws Exception {
+		// Special character will be ignored, only valid index will be put to statistics
+		qscUndnemoCommunicator.setChannelIndex("1,2");
+		qscUndnemoCommunicator.getMultipleStatistics();
+		// Sleep here to wait for worker thread to fetch channel info
+		Thread.sleep(30000);
+		qscUndnemoCommunicator.getMultipleStatistics();
+		ControllableProperty property = new ControllableProperty();
+		property.setValue(2);
+		property.setProperty(QSCUndnemoMetric.ACTIVE_CHANNEL_INDEX.getName());
+		qscUndnemoCommunicator.controlProperty(property);
+		ExtendedStatistics extendedStatistics = (ExtendedStatistics) qscUndnemoCommunicator.getMultipleStatistics().get(0);
+		Map<String, String> stats = extendedStatistics.getStatistics();
+		Assertions.assertEquals("2", stats.get("ActiveChannel#ChannelIndex"));
+		// Set it back to 3, so that it won't conflict other tests.
+		property.setValue(3);
+		property.setProperty(QSCUndnemoMetric.ACTIVE_CHANNEL_INDEX.getName());
+		qscUndnemoCommunicator.controlProperty(property);
 	}
 }
